4a4e27bf35a1139728500996f774fd84afedfe51
Vlad Burtsev <burthev04@gmail.com>
2024-04-20 23:03:44 +0300

    Added useStaticAssets

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,11 @@ import { ConfigService } from '@nestjs/config';
 import * as cookieParser from 'cookie-parser';
 import { ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import * as path from 'path';
+import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
   const configService = app.get(ConfigService);
   const port = configService.get('port');
@@ -14,12 +16,12 @@ async function bootstrap() {
   app.useGlobalPipes(new ValidationPipe());
   app.use(cookieParser());
   app.setGlobalPrefix('api');
+  app.useStaticAssets(path.join(__dirname, '..', '..', 'static'));
 
   const config = new DocumentBuilder()
     .setTitle('API для проєкту "Система ідентифікації НУБіП"')
     .setDescription('Документація REST API')
     .setVersion('1.0.0')
-    .addTag('cursovoy')
     .build();
 
   const document = SwaggerModule.createDocument(app, config);
