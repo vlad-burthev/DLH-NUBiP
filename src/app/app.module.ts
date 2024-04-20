57e68405324a10b113d56574e9430b915ec88793
Vlad Burtsev <burthev04@gmail.com>
2024-04-20 23:02:47 +0300

    Modyfied app.module: added serverFtaticModule, corpuses & corpusesImage: service, controllers, modules and models

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,16 @@ import { SequelizeModule } from '@nestjs/sequelize';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import * as path from 'path';
 import configurations from 'src/config';
+import { CorpusesImageService } from 'src/modules/corpuses-images/corpuses-image.service';
+import { CorpusesImageModule } from 'src/modules/corpuses-images/corpuses-images.module';
+import { CorpusesImage } from 'src/modules/corpuses-images/model/corpuses-image.module';
+import { CorpusesController } from 'src/modules/corpuses/corpuses.controller';
+import { CorpusesModule } from 'src/modules/corpuses/corpuses.module';
+import { CorpusesService } from 'src/modules/corpuses/corpuses.service';
+import { Corpuses } from 'src/modules/corpuses/model/corpuses.model';
+import { FacultiesController } from 'src/modules/faculties/faculties.controller';
 import { FacultiesModule } from 'src/modules/faculties/faculties.module';
+import { FacultiesService } from 'src/modules/faculties/faculties.service';
 import { Faculties } from 'src/modules/faculties/model/faculties.model';
 import { Users } from 'src/modules/users/model/users.model';
 import { UsersController } from 'src/modules/users/users.controller';
@@ -22,9 +31,9 @@ import { UsersService } from 'src/modules/users/users.service';
       global: true,
       secret: process.env.SECRET_KEY,
     }),
-    // ServeStaticModule.forRoot({
-    //   rootPath: path.resolve(__dirname, '..', '..', 'static'),
-    // }),
+    ServeStaticModule.forRoot({
+      rootPath: path.resolve(__dirname, '..', '..', 'static'),
+    }),
     SequelizeModule.forRootAsync({
       imports: [ConfigModule],
       inject: [ConfigService],
@@ -37,13 +46,20 @@ import { UsersService } from 'src/modules/users/users.service';
         database: configService.get<string>('db_name'),
         synchronize: true,
         autoLoadModels: true,
-        models: [Users, Faculties],
+        models: [Users, Faculties, Corpuses, CorpusesImage],
       }),
     }),
     UsersModule,
     FacultiesModule,
+    CorpusesModule,
+    CorpusesImageModule,
   ],
-  controllers: [UsersController],
-  providers: [UsersService],
+  controllers: [UsersController, FacultiesController, CorpusesController],
+  providers: [
+    UsersService,
+    FacultiesService,
+    CorpusesService,
+    CorpusesImageService,
+  ],
 })
 export class AppModule {}
